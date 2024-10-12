9edc0e919f4791091763f8bedd0917939d11fa3b
vadik <baLOZ@GMAIL.COM>
2024-10-12 09:40:53 +0700

    Initial Commit

---
+++ Redi_Express/Redi_ExpressApp.swift
@@ -0,0 +1,17 @@
+//
+//  Redi_ExpressApp.swift
+//  Redi_Express
+//
+//  Created by user on 12.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct Redi_ExpressApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
